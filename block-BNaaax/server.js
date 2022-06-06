407179cee3212ec1e73f82907a81555605c74e85
Baljit Khabra <baljitkhabra99@gmail.com>
2022-06-05 22:25:15 -0700

    complete

---
+++ block-BNaaax/server.js
@@ -0,0 +1,9 @@
+const path = require("path");
+const indexPath = path.join(__dirname, "index.js");
+
+
+var relativePath = './form.html'
+var absolutePath = __dirname;
+var formPath = path.join(__dirname, '/form.html')
+
+console.log(__dirname + '/form.html')
